02e8e60e5efa51ab392ac528ff8c8c02649b315f
xwace <37037519+xwace@users.noreply.github.com>
2023-12-28 10:59:41 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,15 @@
 
 int main(int argc, char *argv[])
 {
-	printf("build time:%d-%d-%d, author:%s\n", BUILD_TIME_YEAR, BUILD_TIME_MONTH, BUILD_TIME_DAY, BUILD_AUTHOR);
+    printf("build time:%d-%d-%d, author:%s\n", BUILD_TIME_YEAR, BUILD_TIME_MONTH, BUILD_TIME_DAY, BUILD_AUTHOR);
     printf("debug level:%d\n", DEBUG_LEVEL);
+	
 #if MEM_CHECKER
     printf("use memory checker\n");
 #endif
+    
+    printf("BUILD_USER: %s",BUILD_USER);
 
-	return 0;
+    return 0;
 }
 
